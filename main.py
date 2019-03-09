d13a631af361449011c1d468b3a091e0e8c43804
Youngyo <youngyona@gmail.com>
2019-03-09 13:52:30 -0500

    set up flask
    set up flask

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 import json
 
-r = requests.get('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?days=20')
-print(json.dumps(r.json()))
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def test():
+    r = requests.get('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?days=20')
+    return json.dumps(r.json())
+
+
+
+if __name__ == '__main__':
+    app.run()
