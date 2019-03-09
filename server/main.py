23e51854c461eeb3741e562f769b6e809c8d1d28
Youngyo <youngyona@gmail.com>
2019-03-09 14:32:10 -0500

    render template
    display home

--- server/main.py
+++ server/main.py
@@ -3,16 +3,16 @@ import requests
 import json
 import os
 
-template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-template_dir = os.path.join(template_dir, 'client')
-template_dir = os.path.join(template_dir, 'templates')
-
-static_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-static_dir = os.path.join(static_dir, 'client')
-static_dir = os.path.join(static_dir, 'static')
+template_dir = "../client/templates"
+static_dir = "../client/static"
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
+def homepage():
+    return render_template('home.html')
+
+
+@app.route('/test', methods = ['GET'])
 def test():
     r = requests.get('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?days=20')
     return json.dumps(r.json())
