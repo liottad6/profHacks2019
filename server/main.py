7c150de907948a3b99f6b931757b329f281311a3
Youngyo <youngyona@gmail.com>
2019-03-09 17:43:54 -0500

    geopy
    convert addresses to coords

--- server/main.py
+++ server/main.py
@@ -12,13 +12,18 @@ app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 def homepage():
     return render_template('home.html')
 
+@app.route('/calculate')
+def hotnessCalculator():
+    location = geolocator.geocode("201 Mullica Hill Rd Glassboro")
+    lat = location.latitude
+    long = location.longitude
+    return str(lat) + str(long)
+
 
 @app.route('/test', methods = ['GET'])
 def test():
     r = requests.get('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?days=20')
     return json.dumps(r.json())
-
-
 
 if __name__ == '__main__':
     app.run()
